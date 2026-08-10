9bc77b13c5cc8b6cb6c91f9ed6315b820e7e735a
Matto4O6 <154467278+Matto4O6@users.noreply.github.com>
2026-08-10 19:20:06 +0100

    Added files for the website
    Files consist of the images, HTML, CSS and JavaScript files.

---
+++ About.js
@@ -0,0 +1,14 @@
+// This JS function uses jquery to fade in images when a button is clicked
+
+$(document).ready(function(){
+    $(".imagebutton").click(function(){
+        $("#sauna").fadeIn(250);
+        $("#PersonalTrainer").fadeIn(500);
+        $("#Cookstown").fadeIn(750);
+        $("#greengym").fadeIn(1000);
+    });
+});
+
+function YourBooking() {
+
+}
